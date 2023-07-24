3e1502e8d34fb9bbec292276a7da5236863592ae
Sarmad Gardezi <mail@sarmadgardezi.com>
2023-07-24 21:46:43 +0500

    all updated

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -1,56 +1,15 @@
 import React, { FC } from 'react';
-import { GetStaticProps } from 'next';
-import { getMDXComponent } from 'mdx-bundler/client';
 
-import { getAllProjects, getProject } from '@lib/mdx/projects';
 
-export type EnrichedProjectMatter = Omit<ProjectMatter, 'images'> & {
-  images: { src: string; placeholder: string; height: number; width: number }[];
-  slug: string;
-  path: string;
-};
-
-export type ProjectMatter = {
-  name: string;
-  website?: string;
-  role?: string;
-  period?: string;
-  category?: string;
-  emoji?: string;
-  images?: string[];
-  summary?: string;
-  domain?: string[];
-  published?: boolean;
-};
-
-export type ProjectProps = {
-  code: string;
-  frontmatter: EnrichedProjectMatter;
-};
-
-const Project: FC<ProjectProps> = ({ code, frontmatter }) => {
-  const MDXBody = React.useMemo(() => getMDXComponent(code), [code]);
+const Project: NextPage<HomePageProps> = props => {
 
   return (
     <div>
-      <h1>{frontmatter.name}</h1>
-      <img src={frontmatter.images?.[0]?.src} alt="Project Image" /> {/* Render the src property from images */}
-      <MDXBody />
+      <h1>Projects</h1>
+      
     </div>
   );
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  const slug = context.params?.slug;
-  if (!slug || Array.isArray(slug)) return { props: {} }; // Return an empty object if slug is invalid
-  const project = await getProject(slug);
-  return { props: { code: project?.code || '', frontmatter: project?.frontmatter || {} } };
-};
-
-export const getStaticPaths = async () => {
-  const projects = await getAllProjects();
-  const paths = projects.map(({ slug }) => ({ params: { slug } }));
-  return { paths, fallback: false };
-};
 
 export default Project;
